9783a4f340ba3c75e46dd1f00a3cbfce18793c8d
Mab01c <mateo.acevedob@upb.edu.co>
2024-08-22 07:45:31 -0500

    Update App.java

--- app/src/main/java/parcial1g1/App.java
+++ app/src/main/java/parcial1g1/App.java
@@ -1,24 +1,55 @@
+package parcial;
 
-package parcial1g1;
+import java.util.Scanner;
 
 public class App {
 
     public static void main(String[] args) {
+    
+    Scanner sc = new Scanner(System.in);
         
-        // Implementar la logica de solucion
-        
-    }
-
-    /*  Implemente una funcion llamada calcularConsumoGasolina que cumpla con las siguientes caracteristicas
-        * Recibe: 
-            - Un float representando los km recorridos
-        * Retorna:
-            - Un float representando los litros de gasolina consumidos
-    */
-    //------------------------------------------------------------------
-    //Implemente la funcion en este espacio
     
 
-    //------------------------------------------------------------------
+    try{
+        
+        System.out.println("¿Cuantos vehiculos participaron?");
+        
+        float kmRecorrido= 0;
+        float ConsumoGasolina = 0;
+        int metrosRecorridos =0;
+        int VParticipacion = sc.nextInt();
+        float ConsumoTotal = 0;
+
+        for(int i=VParticipacion; i > 0; i= i-1){
+           
+            System.out.println("¿Cuantos metros recorrio el carro " + i + "?"); 
+
+             metrosRecorridos = sc.nextInt();
+             kmRecorrido= metrosRecorridos/1000;
+
+             ConsumoGasolina = calcularConsumoGasolina(kmRecorrido);
+
+            System.out.println("Vehiculo: "+ i +" ----- "+kmRecorrido+"Km"+" --> "+ ConsumoGasolina + " Litros de gasolina");
+
+            ConsumoTotal += ConsumoGasolina;
+
+        }
+
+        System.out.println("Consumo total de gasolina por los "+VParticipacion+" Vehiculos es " + ConsumoTotal);
+ 
+        
+
+    }
+    catch(Exception e){
+        System.out.print("Error " + e.getMessage());
+    }
+
+    
+    }
+        
+    public static float calcularConsumoGasolina(float kmRecorrido){
+        return ((kmRecorrido * 55) / 750);
+    }
+    
 
 }
